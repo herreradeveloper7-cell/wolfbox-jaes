1b8a4a7fc34fa1bace7c927095aa733d71f3ddea
herreradeveloper7-cell <herreradeveloper7@gmail.com>
2026-09-15 11:17:17 -0500

    fix: actualizar conexion de base de datos para produccion

--- config/db.js
+++ config/db.js
@@ -49,7 +49,7 @@ const connectWithRetry = async (attempt = 1) => {
     });
 
     pool = await nextPool.connect();
-    console.log("Conectado a Azure SQL");
+    console.log("Conectado SQL server Wolfbox");
     return pool;
   } catch (error) {
     await closePool();
